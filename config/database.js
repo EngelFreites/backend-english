5e9ec4215ca5588e7eaebdb7dc9ecc3e5e6ae6f0
Engelbert <engelfreites97@gmail.com>
2025-04-20 15:35:47 -0300

    refactor: simplify database connection configuration

--- config/database.js
+++ config/database.js
@@ -1,22 +1,22 @@
 const path = require("path");
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: "postgres",
+module.exports = ({ env }) => {
+  const connectionString = env("DATABASE_URL");
+
+  if (!connectionString) {
+    throw new Error("DATABASE_URL is required");
+  }
+
+  return {
     connection: {
-      connectionString: env("DATABASE_URL"),
+      client: "postgres",
+      connection: connectionString,
       ssl: env.bool("DATABASE_SSL", false),
-      host: env("DATABASE_HOST", "localhost"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USERNAME"),
-      password: env("DATABASE_PASSWORD"),
-      schema: "public",
+      pool: {
+        min: env.int("DATABASE_POOL_MIN", 2),
+        max: env.int("DATABASE_POOL_MAX", 10),
+      },
+      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
     },
-    pool: {
-      min: env.int("DATABASE_POOL_MIN", 2),
-      max: env.int("DATABASE_POOL_MAX", 10),
-    },
-    acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
-  },
-});
+  };
+};
