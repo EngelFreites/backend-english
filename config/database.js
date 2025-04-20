14dbbb45f4c6a0c23c4420e11181669e85b0cec4
Engelbert <engelfreites97@gmail.com>
2025-04-20 15:17:45 -0300

    fix: simplify database configuration to use connection string

--- config/database.js
+++ config/database.js
@@ -4,11 +4,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "localhost"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "strapi"),
-      password: env("DATABASE_PASSWORD", "strapi"),
+      connectionString: env("DATABASE_URL"),
       ssl: env.bool("DATABASE_SSL", false),
     },
     pool: {
