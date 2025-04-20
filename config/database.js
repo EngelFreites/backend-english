51ca84fa0645f6f68243006e57ae0505e8a67c37
Engelbert <engelfreites97@gmail.com>
2025-04-20 15:30:07 -0300

    fix: improve database connection configuration

--- config/database.js
+++ config/database.js
@@ -6,15 +6,17 @@ module.exports = ({ env }) => ({
     connection: {
       connectionString: env("DATABASE_URL"),
       ssl: env.bool("DATABASE_SSL", false),
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
+      schema: "public",
     },
     pool: {
       min: env.int("DATABASE_POOL_MIN", 2),
       max: env.int("DATABASE_POOL_MAX", 10),
     },
+    acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
   },
 });
