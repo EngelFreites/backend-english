554f82d0663dc7b8378fd6498f4ed72125e0b8db
Engelbert <engelfreites97@gmail.com>
2025-04-20 15:25:14 -0300

    fix: update database configuration for Render deployment

--- config/database.js
+++ config/database.js
@@ -6,6 +6,11 @@ module.exports = ({ env }) => ({
     connection: {
       connectionString: env("DATABASE_URL"),
       ssl: env.bool("DATABASE_SSL", false),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
     },
     pool: {
       min: env.int("DATABASE_POOL_MIN", 2),
